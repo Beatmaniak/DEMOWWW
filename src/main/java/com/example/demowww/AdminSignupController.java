3171c5462ffbc4956ac987496b4c8d5709d1b7d6
Konrad <brzydli@wp.pl>
2022-05-24 12:52:48 +0200

    jestem zjebany

--- src/main/java/com/example/demowww/AdminSignupController.java
+++ src/main/java/com/example/demowww/AdminSignupController.java
@@ -1,12 +1,24 @@
 package com.example.demowww;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 @Controller
 public class AdminSignupController{
+    private UserService service;
 
+    @RequestMapping("/")
+    public String viewHomePage(Model model) {
+        List<User> userList = service.usersAll();
+        model.addAttribute("userList",userList);
+
+        return "adminsignup";
+    }
     @GetMapping("/adminsignup")
     public String login(@RequestParam(name="name", required=false, defaultValue="World") String name) {
         return "adminsignup";
--- src/main/java/com/example/demowww/UserController.java
+++ src/main/java/com/example/demowww/UserController.java
@@ -4,9 +4,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 @RequestMapping("/api/")
@@ -15,16 +18,29 @@ public class UserController{
     private UserRepository userRepository;
     public UserController(UserRepository userRepository){this.userRepository = userRepository;}
 
+    @RequestMapping(value = "/getUsers", method = RequestMethod.GET)
+    public String getUsers(Model model) throws Exception{
+        List<User> users=getListofUsers();
+        UserListContainer userList= new UserListContainer();
+        userList.setUsers(users);
+        model.addAttribute("Users",userList);
+        return "showUsers";
+    }
+
+    private List<User> getListofUsers() {
+        List<User> users= new ArrayList<User>();
+        return users;
+    }
 
 
     @Autowired
     private UserService userService;
 
-    @PostMapping("users")
-    public ResponseEntity<User> createUser(@Valid @RequestBody User user){
-        User savedUser= userService.createUser(user);
-        return new ResponseEntity<User>(savedUser,HttpStatus.CREATED);
-    }
+//    @PostMapping("users")
+//    public ResponseEntity<User> createUser(@Valid @RequestBody User user){
+//        User savedUser= userService.createUser(user);
+//        return new ResponseEntity<User>(savedUser,HttpStatus.CREATED);
+//    }
 
 }
 
